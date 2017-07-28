22e952152e8b196df0470f7fb6aa3e3c3462f0cb
dciangot <dciangot@cern.ch>
2017-07-28 20:34:37 +0200

    request handler v1

--- getter/getter.go
+++ getter/getter.go
@@ -1,5 +1,12 @@
 package getter
 
+import (
+	"crypto/tls"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 // GetFiles ...
 func GetFiles(ch chan []byte, i []byte) {
 	ch <- i
@@ -8,4 +15,40 @@ func GetFiles(ch chan []byte, i []byte) {
 // ProvidePublication ...
 func ProvidePublication(test []byte) ([]byte, error) {
 	return test, nil
+}
+
+// RequestHandler ...
+func RequestHandler(url string, uri string, verb string, cert string, key string) (string, error) {
+
+	// Load client cert
+	certificate, err := tls.LoadX509KeyPair(cert, key)
+	if err != nil {
+		return "", err
+	}
+
+	tlsConfig := &tls.Config{
+		Certificates:       []tls.Certificate{certificate},
+		InsecureSkipVerify: true,
+	}
+
+	tlsConfig.BuildNameToCertificate()
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{Transport: transport}
+
+	req, _ := http.NewRequest(verb, url+uri, nil)
+
+	req.Header.Set("Accept", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Dump response
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	log.Println(string(data))
+	return string(data), nil
 }
--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"strconv"
 
 	"github.com/microASO/starter/getter"
 )
 
+var (
+	certFile = flag.String("cert", "usercert.pem", "A PEM eoncoded certificate file.")
+	keyFile  = flag.String("key", "unencrypted.pem", "A PEM encoded private key file.")
+)
+
 func main() {
 	log.Print("Processing Started\n")
+	flag.Parse()
+	log.Printf("Using certfile: %s and keyfile: %s", *certFile, *keyFile)
+
+	// define query endpoint
+	url := "https://cmsweb-testbed.cern.ch/crabserver/preprod/filetransfers"
+	data := "subresource=acquiredTransfers&asoworker=asodciangot1&grouping=0"
+
+	// make request
+	response, err := getter.RequestHandler(url, "?"+data, "GET", *certFile, *keyFile)
+	if err != nil {
+		log.Fatalf("Error retrieving publication with %s", url+"?"+data+"\n"+err.Error())
+	}
+
+	log.Print(response)
+	// TODO: convert response json
+
 	ch := make(chan []byte, 100)
 
 	for i := 0; i <= 10; i++ {
